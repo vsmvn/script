438a1d59cc6fb7cdc5b1de0a2ea72d04ddc98b0f
Vsm.vn【Digital Marketing】 <97682951+vsmvn@users.noreply.github.com>
2022-04-02 22:01:57 +0700

    Update traffic-download.js

--- traffic-download.js
+++ traffic-download.js
@@ -1,6 +1,6 @@
 function generate() {
   var e, n = document.getElementById("downloadx"),
-    l = 65,
+    l = 75,
     d = document.createElement("span");
   n.parentNode.replaceChild(d, n), e = setInterval(function() {
     --l < 0 ? (d.parentNode.replaceChild(n, d), clearInterval(e), n.style.display = "block") : (d.innerHTML = "<center>Vui lòng chờ " + l.toString() + " giây...</center>")
